639196e16d28f84cd8d7f4cc40cfcf77adcf4127
Babul Akter <babulakterfsd@gmail.com>
2023-06-05 16:27:28 +0600

    footer responsive done.

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
   return (
     <div className="container relative">
       <div className="hidden md:block bg-[#040404] py-8 md:py-14"></div>
-      <div className=" bg-[#040404] md:bg-[#060d18] pt-10 md:pt-0 md:py-6 flex flex-col md:gap-y-20 justify-center items-center mt-12 md:mt-0 px-4 md:px-0">
+      <div className=" bg-[#040404] md:bg-[#060d18] pt-12 md:pt-0 md:py-6 flex flex-col md:gap-y-20 justify-center items-center mt-12 md:mt-0 px-4 md:px-0">
         <div className="bg-[#171b29] rounded-xl md:p-4 -mt-14 md:w-10/12">
           <div className="gradientBackground px-1 md:px-6 py-6 rounded-xl flex flex-col md:flex-row justify-evenly items-center">
             <h2
