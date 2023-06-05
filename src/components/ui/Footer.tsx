191a8d90654de479b79beb9d2df5326cd43667c3
Babul Akter <babulakterfsd@gmail.com>
2023-06-05 12:15:44 +0600

    footer responsive done

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -9,18 +9,31 @@ import Styles from '../../styles/footer.module.css';
 
 const Footer = () => {
   return (
-    <div className="bg-[#171B29] md:pt-48 md:pb-3">
-      <div className="grid grid-cols-12 md:px-20">
+    <div className="bg-[#171B29] pt-20 md:pt-48 pb-3">
+      <div className="grid grid-cols-12 mx-2 md:px-20">
         <div className="col-span-12 lg:col-span-4">
-          <div className="flex">
-            <Image src={logo} alt="logo" height={52} width={275} />
+          <div className="flex justify-center md:justify-start items-center md:items-start">
+            <Image
+              src={logo}
+              alt="logo"
+              height={52}
+              width={275}
+              className="hidden md:block"
+            />
+            <Image
+              src={logo}
+              alt="logo"
+              height={50}
+              width={200}
+              className="block md:hidden"
+            />
           </div>
-          <p className="text-sm md:mt-6 md:ml-5">
+          <p className="text-center md:text-left text-sm md:mt-6 md:ml-5 mb-8 md:mb-0 mt-2">
             Lorem ipsum dolor sit amet consectetur. Lobortis duis duis sed sit
             integer mauris gravida proin eget.
           </p>
           <div className="hidden md:flex px-0.5 justify-center items-center flex-col gap-y-5">
-            <div className="flex flex-col lg:ml-5 mt-16">
+            <div className="flex flex-col lg:ml-5 mt-14">
               <h4 className=" font-semibold text-gray-200 my-1 lg:ml-5 text-md ">
                 Join Waitlist!
               </h4>
@@ -50,15 +63,15 @@ const Footer = () => {
           </div>
         </div>
         <div className="col-span-12 lg:col-span-8 md:px-8 md:ml-auto">
-          <div className="flex justify-center items-center md:gap-x-32">
+          <div className="flex justify-center items-center gap-x-16 md:gap-x-32">
             <div className="grid-cols-1 lg:grid-cols-3">
               <h6
-                className="text-center md:mb-7"
+                className="text-center mb-1 md:mb-5"
                 style={{ fontFamily: 'adieuregular', letterSpacing: '1.5%' }}
               >
                 Product
               </h6>
-              <div className="flex flex-col md:gap-y-3 text-center">
+              <div className="flex flex-col gap-y-1.5 md:gap-y-3 text-center text-sm">
                 <span>Overons</span>
                 <span>Integration</span>
                 <span>Docs</span>
@@ -69,12 +82,12 @@ const Footer = () => {
             </div>
             <div className="grid-cols-1 lg:grid-cols-3">
               <h6
-                className="text-center md:mb-7"
+                className="text-center mb-1 md:mb-5"
                 style={{ fontFamily: 'adieuregular', letterSpacing: '1.5%' }}
               >
                 company
               </h6>
-              <div className="flex flex-col md:gap-y-3 text-center">
+              <div className="flex flex-col gap-y-1.5 md:gap-y-3 text-center text-sm">
                 <span>About Us</span>
                 <span>Careers</span>
                 <span>Brand</span>
@@ -83,14 +96,14 @@ const Footer = () => {
                 <span>Blog</span>
               </div>
             </div>
-            <div className="grid-cols-1 lg:grid-cols-3">
+            <div className="grid-cols-1 lg:grid-cols-3 hidden md:block">
               <h6
-                className="text-center md:mb-7"
+                className="text-center mb-5"
                 style={{ fontFamily: 'adieuregular', letterSpacing: '1.5%' }}
               >
                 Resources
               </h6>
-              <div className="flex flex-col md:gap-y-3 text-center">
+              <div className="flex flex-col md:gap-y-3 text-center text-sm">
                 <span>Developer</span>
                 <span>API</span>
                 <span>QAI</span>
@@ -103,16 +116,16 @@ const Footer = () => {
         </div>
       </div>
       {/* divider */}
-      <div className="flex justify-center items-center md:mt-16">
+      <div className="flex justify-center items-center mt-8 md:mt-16">
         <div className="w-[90%] h-[1px] bg-[#687686]"></div>
       </div>
-      <div className="flex justify-between items-center md:px-20">
-        <div className="flex md:gap-x-6 md:pt-4">
+      <div className="flex flex-col md:flex-row justify-between items-center px-6 md:px-20">
+        <div className="flex gap-x-6 pt-2 md:pt-4">
           <span className="text-sm">Terms</span>
           <span className="text-sm">Privacy</span>
           <span className="text-sm">Cookies</span>
         </div>
-        <div className="socialmediaicons flex md:gap-x-6 md:pt-4">
+        <div className="socialmediaicons flex gap-x-6 pt-4 pb-4 md:pb-0">
           <Image
             src={insta}
             alt="insta"
