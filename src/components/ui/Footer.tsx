4f2e340511835840e30c0fa019383a61b0da57a5
Babul Akter <babulakterfsd@gmail.com>
2023-06-05 11:21:22 +0600

    horizontal scrollbar removed from mobile

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -9,13 +9,13 @@ import Styles from '../../styles/footer.module.css';
 
 const Footer = () => {
   return (
-    <div className="bg-[#171B29] pt-48 pb-3">
-      <div className="grid grid-cols-12 px-20">
+    <div className="bg-[#171B29] md:pt-48 md:pb-3">
+      <div className="grid grid-cols-12 md:px-20">
         <div className="col-span-12 lg:col-span-4">
           <div className="flex">
             <Image src={logo} alt="logo" height={52} width={275} />
           </div>
-          <p className="text-sm mt-6 ml-5">
+          <p className="text-sm md:mt-6 md:ml-5">
             Lorem ipsum dolor sit amet consectetur. Lobortis duis duis sed sit
             integer mauris gravida proin eget.
           </p>
@@ -49,16 +49,16 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-12 lg:col-span-8 px-8 ml-auto">
-          <div className="flex justify-center items-center gap-x-32">
+        <div className="col-span-12 lg:col-span-8 md:px-8 md:ml-auto">
+          <div className="flex justify-center items-center md:gap-x-32">
             <div className="grid-cols-1 lg:grid-cols-3">
               <h6
-                className="text-center mb-7"
+                className="text-center md:mb-7"
                 style={{ fontFamily: 'adieuregular', letterSpacing: '1.5%' }}
               >
                 Product
               </h6>
-              <div className="flex flex-col gap-y-3 text-center">
+              <div className="flex flex-col md:gap-y-3 text-center">
                 <span>Overons</span>
                 <span>Integration</span>
                 <span>Docs</span>
@@ -69,12 +69,12 @@ const Footer = () => {
             </div>
             <div className="grid-cols-1 lg:grid-cols-3">
               <h6
-                className="text-center mb-7"
+                className="text-center md:mb-7"
                 style={{ fontFamily: 'adieuregular', letterSpacing: '1.5%' }}
               >
                 company
               </h6>
-              <div className="flex flex-col gap-y-3 text-center">
+              <div className="flex flex-col md:gap-y-3 text-center">
                 <span>About Us</span>
                 <span>Careers</span>
                 <span>Brand</span>
@@ -85,12 +85,12 @@ const Footer = () => {
             </div>
             <div className="grid-cols-1 lg:grid-cols-3">
               <h6
-                className="text-center mb-7"
+                className="text-center md:mb-7"
                 style={{ fontFamily: 'adieuregular', letterSpacing: '1.5%' }}
               >
                 Resources
               </h6>
-              <div className="flex flex-col gap-y-3 text-center">
+              <div className="flex flex-col md:gap-y-3 text-center">
                 <span>Developer</span>
                 <span>API</span>
                 <span>QAI</span>
@@ -103,16 +103,16 @@ const Footer = () => {
         </div>
       </div>
       {/* divider */}
-      <div className="flex justify-center items-center mt-16">
+      <div className="flex justify-center items-center md:mt-16">
         <div className="w-[90%] h-[1px] bg-[#687686]"></div>
       </div>
-      <div className="flex justify-between items-center px-20">
-        <div className="flex gap-x-6 pt-4">
+      <div className="flex justify-between items-center md:px-20">
+        <div className="flex md:gap-x-6 md:pt-4">
           <span className="text-sm">Terms</span>
           <span className="text-sm">Privacy</span>
           <span className="text-sm">Cookies</span>
         </div>
-        <div className="socialmediaicons flex gap-x-6 pt-4">
+        <div className="socialmediaicons flex md:gap-x-6 md:pt-4">
           <Image
             src={insta}
             alt="insta"
