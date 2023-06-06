a6c48eeb98f3d2c1fc1fdb012c058bb4bf09191f
Babul Akter <babulakterfsd@gmail.com>
2023-06-06 11:39:10 +0600

    api response fixed for mobile

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function GlobalError({
   error,
   reset,
@@ -9,9 +11,13 @@ export default function GlobalError({
 }) {
   return (
     <html>
-      <body>
-        <h2>Something went wrong, Babul!</h2>
-        <button onClick={() => reset()}>Try again</button>
+      <body className="bg-black text-white h-screen flex justify-center items-center ">
+        <div className="bg-[#1c2647] p-4 rounded-xl">
+          <h2>Sorry User, Something went wrong !</h2>
+          <Link href="/">
+            <button className="gradientBackground p-2">Go Back</button>
+          </Link>
+        </div>
       </body>
     </html>
   );
