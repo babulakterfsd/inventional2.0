dcd8f79d6faf10ac63fcb38d9d9ee0a2e2196399
Babul Akter <babulakterfsd@gmail.com>
2023-06-07 10:14:54 +0600

    minor design bugs solved

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
       <div className=" bg-[#040404] md:bg-[#060d18] pt-12 md:pt-0 md:py-6 flex flex-col md:gap-y-20 justify-center items-center mt-12 md:mt-0 px-4 md:px-0">
         <div
           className="bg-[#171b29] rounded-xl md:p-4 -mt-14 md:w-10/12"
-          data-aos="zoom-in"
+          data-aos="fade-down"
           data-aos-duration="1500"
         >
           <div className="gradientBackground px-1 md:px-6 py-6 rounded-xl flex flex-col md:flex-row justify-evenly items-center">
@@ -60,7 +60,7 @@ const Contact = () => {
               letterSpacing: '-0.03rem',
               lineHeight: '1.2',
             }}
-            data-aos="zoom-in"
+            data-aos="fade-up"
             data-aos-duration="1500"
           >
             Let's Build something with <br />
